54c5c69bdae565d4bd00f32d396e38f96e6e5e16
jay <jsureliya732@rku.ac.in>
2026-05-25 19:43:11 +0530

    URL upadte

--- frontend/app.py
+++ frontend/app.py
@@ -159,7 +159,7 @@ if submitted:
 
     try:
         with st.spinner("Analyzing clinical data..."):
-            response = requests.post("https://heart-failure-prediction-project.onrender.com/predict", json=data)
+            response = requests.post("https://heartguard-api-8cxx.onrender.com/predict", json=data)
 
         if response.status_code == 200:
             result = response.json()
